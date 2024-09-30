b91c1acdf15c31fd2d8ecc159e54053402926a11
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 05:00:10 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vNNlQbgI#PYCMry0M_0eacsvxAUI0GbG54-RgdTe9BQn_5esfR1E",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/c5048f76-6400-456e-938a-962ab0111337",
-ALIVE_MSG : process.env.ALIVE_MSG || "*❤‍🩹WIHANGA-MD ALLIVE NOW❤‍🩹*",
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/99312f6ecbfa4d9bfd3b4.jpg",
+ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃WIHANGA-MD 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : WIHANGA ``| 2024🤍*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
