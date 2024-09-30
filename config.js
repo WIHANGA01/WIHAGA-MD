f846f6cdc84ca514d7a0aff07a476103c5c5d121
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 09:49:00 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "vNNlQbgI#PYCMry0M_0eacsvxAUI0GbG54-RgdTe9BQn_5esfR1E",
-MONGODB: process.env.enc.MONGODB || "",
+MONGODB: process.env.enc.MONGODB || "mongodb://mongo:FIZQtJmBhmsWZQdozTkdfSQVDKQSNVjl@autorack.proxy.rlwy.net:42113",
 };
