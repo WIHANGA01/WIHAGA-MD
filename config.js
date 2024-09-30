5c3aed65c705afc883f5375620ef13d7d420a8ec
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-29 23:51:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "vNNlQbgI#PYCMry0M_0eacsvxAUI0GbG54-RgdTe9BQn_5esfR1E",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://chat.wh",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/c5048f76-6400-456e-938a-962ab0111337",
 ALIVE_MSG : process.env.ALIVE_MSG || "*WIHANGA-MD ALLIVE NOW (TYPE MENU TO GET COMMAND LIST)*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
