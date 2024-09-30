65c9d3d308abe465c8dbf61b11b753710b5af330
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 06:19:00 -0800

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,4 +1,4 @@
-const {readEnv} = require('../lib/database')
+const config = require('../config')
 const {cmd , commands} = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
