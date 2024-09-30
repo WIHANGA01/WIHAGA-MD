7b7419f464f731222e910d7df6a1e8e49f36c77c
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 10:01:39 -0800

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,49 +1,123 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
-    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{ 
+try{
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+//============ping=======
+cmd({
+    pattern: "ping",
+    react: "⚡",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '```Pinging To index.js!!!```'  }, { quoted: mek } )
+var final = new Date().getTime();
+return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
+} catch (e) {
+reply(`${e}`)
+console.log(e)
+}
+})
+
+//===========menu========
+cmd({
+    pattern: "menu",
+    react: "📃",
+    alias: ["panel","list","commands"],
+    desc: "Get bot\'s command list.",
+    category: "main",
+    use: '.menu',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let tex = `
+> *👋. *HEY* *FREIND* ジ,
+
+> ɪ ᴀᴍ ᴀɴ ᴀᴜᴛᴏᴍᴀᴛᴇᴅ ꜱʏꜱᴛᴇᴍ ⚡ *(ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ)* ᴛʜᴀᴛ ᴄᴀɴ ʜᴇʟᴘ ᴛᴏ ᴅᴏ ꜱᴏᴍᴇᴛʜɪɴɢ, ꜱᴇᴀʀᴄʜ ᴀɴᴅ ɢᴇᴛ ᴅᴀᴛᴀ / ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ᴏɴʟʏ ᴛʜʀᴏᴜɢʜ *ᴡʜᴀᴛꜱᴀᴘᴘ.🌐*
+
+🦄 ∆ ◦ _*ɴᴀᴍᴇ ʙᴏᴛ*_ : 💃WIHANGA-MD 🤍
+🦄 ∆ ◦ _*ᴄʀᴇᴀᴛᴏʀ*_ : *WIHAnGA* ジ 🤍
+🦄 ∆ ◦ _*ᴠᴇʀsɪᴏɴs*_ : 1.0.0 ( *ᴀᴅᴅᴇᴅ ʜɪᴅᴅᴇɴ ғᴇᴀᴜᴛᴜʀᴇs* )🤍
+🦄 ∆ ◦ _*ᴛʏᴘᴇ sᴄʀɪᴘᴛ*_ : *ᴘʟᴜɢɪɴs* 🤍
+🦄 ∆ ◦ _*ᴊᴏɪɴ ᴍʏ ɢʀᴏᴜᴘ*_ :  ~https://whatsapp.com/channel/0029Va8f3smKWEKvPUzXQv34~  🤍
+🦄 ∆ ◦ _*ᴄᴏɴᴛᴀᴄᴛ*_ : ~https://wa.me/94740460412~ 🤍
+
+> *ʜɪ 💗, ʙᴇғᴏʀᴇ ɢᴏɪɴɢ ᴛᴏ ᴛʜᴇ ғᴇᴀᴛᴜʀᴇs ᴍᴇɴᴜ 📁, ᴘʟᴇᴀsᴇ ʀᴇᴀᴅ ᴛʜᴇ ʀᴜʟᴇs ғᴏʀ ᴜsɪɴɢ ᴛʜᴇ ʙᴏᴛ* 💠
 
 
-let des = `*👨‍💻 Hello ${pushname}*
 
-⚡𝗛𝗘𝗟𝗟𝗢𝗪 ┇ 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 ⚡
+┌ 🅥 *ＤＯＷＮＬＯＡＤＥＲ* 🅥
+│◦ .ғʙ <ᴜʀʟ> 🌸
+│◦ .ᴀᴘᴋ <ϙᴜᴇʀʏ> 🌸
+│◦ .ɪɢ <ᴜʀʟ> 🌸
+│◦ .ᴠɪᴅᴇᴏ <ϙᴜᴇʀʏ> 🌸
+│◦ .ᴛɪᴋᴛᴏᴋ <ᴜʀʟ> 🌸
+│◦ .ᴍᴇᴅɪᴀғɪʀᴇ <ᴜʀʟ> 🌸
+│◦ .ꜱᴏɴɢ <ϙᴜᴇʀʏ> 🌸
+│◦ .ᴠɪᴅᴇᴏ <ϙᴜᴇʀʏ> 🌸
+└ ◦ .ʏᴛs <ϙᴜᴇʀʏ> 🌸
 
-👋🏻𝗪𝗜𝗛𝗔𝗡𝗚𝗔-𝗠𝗗 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪 👋🏻
-┗━━━━━━━━━━◯
+┌ 🅥 *ＡＩ* 🅥
+└ ◦ .ᴀɪ <ᴛᴇxᴛ> 🌸
+
+┌ 🅥 *ＯＷＮＥＲ* 🅥
+│◦ .ʙᴀɴᴄʜᴀᴛ 🌸
+│◦ .ʙᴀɴ @ᴜsᴇʀ 🌸
+│◦ .ᴜɴʙᴀɴ 🌸
+│◦ .ᴜɴʙᴀɴ @ᴜsᴇʀ 🌸
+│◦ .ʙʟᴏᴄᴋ 🌸
+│◦ .ᴜɴʙʟᴏᴄᴋ 🌸
+│◦ >/=> 🌸
+│◦ $ 🌸
+│◦ .sᴇᴛᴘᴘ <ʀᴇᴘʟʏ ᴡɪᴛʜ ᴘʜᴏᴛᴏ> 🌸
+└ ◦ .sᴇᴛʙɪᴏ 🌸
+
+┌ 🅥 *ＧＲＯＵＰ* 🅥
+│◦ .ᴅᴇʟᴇᴛᴇ <ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ᴍsɢ ᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟ> 🌸
+│◦ .sᴇᴛᴅᴇsᴄ <ᴛᴇxᴛ> 🌸
+│◦ .ᴘʀᴏᴍᴏᴛᴇ 🌸
+│◦ .ᴅᴇᴍᴏᴛᴇ 🌸
+│◦ .ʜɪᴅᴇᴛᴀɢ 🌸
+│◦ .ɪɴᴠɪᴛᴇ 🌸
+│◦ .ᴋɪᴄᴋ 🌸
+└ ◦ .ʟɪɴᴋ 🌸
+
+┌ 🅥 *ＩＮＦＯ* 🅥
+│◦ .ᴍᴇɴᴜ 🌸
+│◦ .ᴘɪɴɢ 🌸
+│◦ .ꜱʏꜱᴛᴇᴍ 🌸
+└ ◦ .ᴀʟɪᴠᴇ 🌸
+
+┌ 🅥 *ＣＯＮＶＥＲＴＥＲ* 🅥
+│◦ .ᴛᴏᴍᴘ3 🌸
+│◦ .ᴛᴏᴜʀʟ 🌸
+└ ◦ .sᴛɪᴄᴋᴇʀ 🌸
+
+*⚖️°𝙱𝚢 © Ｄａｒｋ Ｎｅｏｎ Ｃｙｂｅｒｚ •2024•* 💗
+`
 
 
-
-
-
-
-
-┏━━━━━━━━━━
-↪️ 𝗧𝗬𝗣𝗘 𝗧𝗢 .𝗺𝗲𝗻𝘂 𝗚𝗘𝗧 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧 ⃟
-┃
-┃━━━━━━━━━━━
-┃
-↪️ 𝗧𝗬𝗣𝗘 𝗧𝗢 .𝘀𝘆𝘀𝘁𝗲𝗺 𝗚𝗘𝗧 𝗕𝗢𝗧 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢⃟
-┃
-┃━━━━━━━━━━━
-┃
-↪️ 𝗙𝗔𝗦𝗧 𝗪𝗛𝗔𝗧𝗦 𝗔𝗣𝗣 𝗕𝗢𝗧
-┃
-┃━━━━━━━━━━━
-┃
-꧁⚒️ ᴡɪʜᴀɴɢᴀ-ᴍᴅ ⚒️꧂
-┃
-┗━━━━━━╋━━━━━`
-
-
-return await conn.sendMessage(from,{image: {url: `https://i.imgur.com/oILqBWB.jpeg`},caption: des},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: tex},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
