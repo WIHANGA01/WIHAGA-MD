1cc0f9bc4ee7e7047db8864594cf72d563973723
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 00:01:56 -0800

    Create fb.js

---
+++ plugins/fb.js
@@ -0,0 +1,57 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')	
+const fs = require('fs')
+const fg = require('api-dylux');
+
+
+cmd({
+  pattern: "fb",
+  react: "⏳",
+  category: "download",
+  filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, prefix, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!args[0]) {
+        throw ` Please send the link of a Facebook video\n\nEXAMPLE :\n *${prefix + command}* https://fb.watch/7B5KBCgdO3`;
+    }
+
+    const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+    if (!urlRegex.test(args[0])) {
+        throw '⚠️ PLEASE GIVE A VALID URL.';
+    }
+     await reply(`Please wait...`);
+    
+        const result = await fg.fbdl(args[0]);
+        const tex = `
+  *Video Details* 
+📽️ *Title*: ${result.title}
+`;
+
+
+        const response = await fetch(result.videoUrl);
+        const arrayBuffer = await response.arrayBuffer();
+        const videoBuffer = Buffer.from(arrayBuffer);
+
+        // Save the videoBuffer to a temporary file
+        const randomName = `temp_${Math.floor(Math.random() * 10000)}.mp4`;
+        fs.writeFileSync(`./${randomName}`, videoBuffer);
+
+        // Send the video using client.sendMessage
+        await conn.sendMessage(
+            from,
+            {
+                video: fs.readFileSync(`./${randomName}`),
+                caption: tex,
+            },
+            { quoted: mek }
+        );
+
+        fs.unlinkSync(`./${randomName}`);
+    } catch (e) {
+        console.log(e);
+        reply('⚠️ An error occurred while processing the request. Please try again later.');
+l(e)
+}
+})
