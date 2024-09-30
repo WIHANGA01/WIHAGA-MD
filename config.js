86da5291459430d82803ba2f8c3353b3b1b1a670
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 10:04:24 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "vNNlQbgI#PYCMry0M_0eacsvxAUI0GbG54-RgdTe9BQn_5esfR1E",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/99312f6ecbfa4d9bfd3b4.jpg",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/ac71a7f5-7272-4d0b-91ca-8461bbf3d5f9",
+ALIVE_MSG : process.env.ALIVE_MSG || "*WIHANGA-MD ALLIVE NOW/n/n/n/ntype .menu get cmd list*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
