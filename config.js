e9481636dc264660d51c40c30f8880f8f678bb15
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-09-30 21:08:39 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "vNNlQbgI#PYCMry0M_0eacsvxAUI0GbG54-RgdTe9BQn_5esfR1E",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/ac71a7f5-7272-4d0b-91ca-8461bbf3d5f9",
-ALIVE_MSG : process.env.ALIVE_MSG || "*WIHANGA-MD ALLIVE NOW*",
+ALIVE_MSG : process.env.ALIVE_MSG || "*WIHANGA-MD ALLIVE NOW |*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
