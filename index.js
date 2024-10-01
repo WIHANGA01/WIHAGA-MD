4447800ec29871f46c6ac417b9695524a8e5470f
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-01 05:54:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const url = 'https://raw.githubusercontent.com/WIHANGA01/AUTO_REPLY/refs/heads/m
 let { data } = await axios.get(url)
 for (vr in data){
 if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
- }
+ }}
         
 //=====================autostiker===============================
 
@@ -161,7 +161,7 @@ if (config.AUTO_STIKER === 'true') {
 const url = 'https://raw.githubusercontent.com/WIHANGA01/AUTO_REPLY/refs/heads/main/auto_stikers'
 let { data } = await axios.get(url)
 for (vr in data){
-if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{stike5: { url : data[txt]},package: 'wihanga-md'},{quoted:mek})   
+conn.sendMessage(from,{stike5: { url : data[txt]},package: 'wihanga-md'},{quoted:mek})   
  }}
 
 
