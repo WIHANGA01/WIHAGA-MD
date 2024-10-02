51f172939c6d101290e31fb64dba3be6764feb27
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-02 04:54:19 -0800

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -51,9 +51,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let tex = `
-👋🏻𝗛𝗘𝗬 𝗠𝗬 ${pushname} 𝗙𝗥𝗜𝗘𝗡𝗗 ッ 👋🏻
+👋🏻𝗛𝗘𝗬 @${pushname} 𝗙𝗥𝗜𝗘𝗡𝗗 ッ 👋🏻
 
-*😹 හෙලොව් මගේ රත්තරං යාලුවේ 😹*
+*😹-welcome WIHANGA-MD-😹*
 
 > ⚖️ 𝙌𝙐𝙄𝙆 𝘽𝙊𝙏 ⚖️
 
