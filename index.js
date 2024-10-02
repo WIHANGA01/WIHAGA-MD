67d5ea587674755d6fe1da8837c9e1cf8137e996
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-02 06:15:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -153,10 +153,10 @@ const url = 'https://raw.githubusercontent.com/WIHANGA01/AUTO_REPLY/refs/heads/m
 let { data } = await axios.get(url)
 for (vr in data){
 if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
- }}
 
+}}
 
-//========OwnerReact========            
+        //========OwnerReact========            
          
 if(senderNumber.includes("94769611502")){
 if(isReact) return
