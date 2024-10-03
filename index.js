7f617cfe6a841cafa6b7fe5e5b86d5cb239a64d0
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-02 23:43:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+
 
 const ownerNumber = ['94760774445']
 
@@ -36,14 +36,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
-//=============================================
+//=========================================================================
 
 async function connectToWA() {
 //=======connectdb=================
 const connectDB = require('./lib/mongodb')
 connectDB();
-//==================================
-
+//=================================================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//===========================================================================
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
