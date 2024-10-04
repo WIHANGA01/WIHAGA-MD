3183ae075a0ce28ea33b2d63d9611d284334cf24
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-04 21:22:38 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,3 +1,4 @@
+
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
@@ -16,28 +17,6 @@ reply(`${e}`)
 }
 })
 
-//============ping=======
-cmd({
-    pattern: "ping",
-    react: "⚡",
-    alias: ["speed"],
-    desc: "Check bot\'s ping",
-    category: "main",
-    use: '.ping',
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '```Pinging To index.js!!!```'  }, { quoted: mek } )
-var final = new Date().getTime();
-return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
-} catch (e) {
-reply(`${e}`)
-console.log(e)
-}
-})
 
 //===========menu========
 cmd({
