f78e765dcc6b74abb81b7dae7dd90f3b7ac05797
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-05 12:40:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ PING_IMG : process.env.PING_IMG || "https://github.com/user-attachments/assets/5
 MENU_IMG : process.env.MENU_IMG || "https://github.com/user-attachments/assets/ad3dc519-0363-455a-a295-6860e942fd07",
 REPO_IMG : process.env.REPO_IMG || "https://github.com/user-attachments/assets/48434a6d-1ecd-4a02-a38f-aad39c7b573c",
 ALIVE_MSG : process.env.ALIVE_MSG || "━━━━━━━━━━━━━●\n\n\👋🏻𝗪𝗜𝗛𝗔𝗡𝗚𝗔-𝗠𝗗 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪👋🏻\n\━━━━━━━━━━━━●\n\n📃𝗧𝗬𝗣𝗘 .ᴍᴇɴᴜ 𝗚𝗘𝗧 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧📃\n\n\━━━━━━━━━━━●\n\n\🔮𝗧𝗬𝗣𝗘 .ᴘɪɴɢ 𝗕𝗢𝗧 𝗦𝗣𝗘𝗘𝗗🔮\n\n\n━━━━━━━━━━●\n\n\n📡𝗧𝗬𝗣𝗘 .ꜱʏꜱᴛᴇᴍ 𝗚𝗘𝗧 𝗕𝗢𝗧 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢📡\n\n\n\n\n\꧁𝗪𝗜𝗛𝗔𝗡𝗚𝗔-𝗠𝗗꧂",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/503d7e96-e525-42b5-950e-935fda65eb5a",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/22d7c634-8bb3-412e-9fca-1be95dcb440f",
 AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || "true",
 AUTO_REPLY : process.env.AUTO_REPLY || "true",
 AUTO_STIKER : process.env.AUTO_STIKER || "true",
