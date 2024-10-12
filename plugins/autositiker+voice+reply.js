f399efdac00de784c7e2da13c7fad933b23ec039
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-11 22:40:42 -0800

    Update autositiker+voice+reply.js

--- plugins/autositiker+voice+reply.js
+++ plugins/autositiker+voice+reply.js
@@ -3,24 +3,7 @@ const path = require('path');
 const config = require('../config')
 const {cmd , commands} = require('../command')
 
-//auto_voice
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_VOICE === 'true') {
-                //if (isOwner) return;        
-                await conn.sendPresenceUpdate('recording', from);
-                await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
-            }
-        }
-    }                
-});
+
 
 //auto sticker 
 cmd({
