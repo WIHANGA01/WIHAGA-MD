22151b11c32e3e67950c99ca1a6e09d874916d69
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-15 20:30:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "F3kUUDga#rdxuX8ah7n4h_Hgx_mOhpcntF972lqIZBfVhrJ0yAUs", 
 MONGODB: process.env.MONGODB || "mongodb://mongo:ZOHuvZUjalWCSIOaChWEbVSLoCdVDPTE@junction.proxy.rlwy.net:42713",
-PING_IMG : process.env.PING_IMG || "https://github.com/user-attachments/assets/5970bd48-8cd1-40e9-8c0d-4fad372a1d89",
-SYS_IMG : process.env.SYS_IMG || "https://github.com/user-attachments/assets/813ecbd6-bf45-4766-a83d-79df76e77f14",
+PING_IMG : process.env.PING_IMG || "https://github.com/user-attachments/assets/578e0865-1fe7-4a4e-affe-7578ff29fe65",
+SYS_IMG : process.env.SYS_IMG || "https://github.com/user-attachments/assets/a0ecdc6e-b06b-4349-a8b7-5237238ca0ce",
 MENU_IMG : process.env.MENU_IMG || "https://github.com/user-attachments/assets/f803fcf9-e171-4096-89a7-8d21c49e912c",
 REPO_IMG : process.env.REPO_IMG || "https://github.com/user-attachments/assets/84ad21e9-b5df-411b-a453-369abd8b4253",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/1db105d7-0068-4879-b1fc-fece3c5f18b4",
