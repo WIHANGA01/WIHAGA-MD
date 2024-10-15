3422db6064677b29da12858e669020b271e165b5
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-10-15 19:12:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ SYS_IMG : process.env.SYS_IMG || "https://github.com/user-attachments/assets/813
 MENU_IMG : process.env.MENU_IMG || "https://github.com/user-attachments/assets/f803fcf9-e171-4096-89a7-8d21c49e912c",
 REPO_IMG : process.env.REPO_IMG || "https://github.com/user-attachments/assets/84ad21e9-b5df-411b-a453-369abd8b4253",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/1db105d7-0068-4879-b1fc-fece3c5f18b4",
-ALIVE_MSG : process.env.ALIVE_MSG || "wihanga-md ALLIVE now\n\n\nHOW CAN HELP YOU...",
+ALIVE_MSG : process.env.ALIVE_MSG || "╭┄┄┄┄┄┄┄┄┄◯\n┆⚙️ 𝙒𝙄𝙃𝘼𝙉𝙂𝘼-𝙈𝘿 𝘼𝙇𝙄𝙑𝙀  ⚙️┆\n┆\n┆\n┆📜𝙏𝙔𝙋𝙀. 𝙢𝙚𝙣𝙪 𝙂𝙀𝙏\n┆𝘾𝙈𝘿 ┆𝙇𝙄𝙎𝙏📜\n┆\n┆🧬𝙏𝙔𝙋𝙀 .𝙥𝙞𝙣𝙜 𝘾𝙃𝙀𝙆 ┆𝙎𝙋𝙀𝙀𝘿🧬\n┆\n┆⏳𝙏𝙔𝙋𝙀 .𝙧𝙪𝙣𝙩𝙮𝙢𝙚 𝘾𝙃𝙀𝙆 ┆𝙍𝙐𝙈𝙏𝙄𝙈𝙀⏳\n┆\n┆⚒️𝘽𝙊𝙏 𝙋𝙍𝙀𝙁𝙄𝙓 ( . )⚒️\n┆\n┆\n┆\n┆🎐◁ ᴡɪʜᴀɴɢᴀ-ᴍᴅ⃞ ㋡\n┆\n╰┄┄┄┄┄┄┄┄┄◯",
 AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || "true",
 AUTO_REPLY : process.env.AUTO_REPLY || "true",
 AUTO_STIKER : process.env.AUTO_STIKER || "true",
